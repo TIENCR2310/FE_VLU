96400d3fcfc3e5e6f0e8f15dc17047ed440a9461
TIENCR2310 <120126874+TIENCR2310@users.noreply.github.com>
2025-01-16 06:33:29 +0700

    apiClient.js

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: " https://cc93-2405-4802-815e-e440-a910-e597-4c20-64a.ngrok-free.app", 
+    baseURL: "https://694d-2405-4802-815e-e440-a910-e597-4c20-64a.ngrok-free.app", 
     headers: {
         "Content-Type": "application/json", 
         "ngrok-skip-browser-warning": "true", 
